c4ca44f4dcd42038e41a50335bc2cd8386985947
didac2003 <94047560+didac2003@users.noreply.github.com>
2021-11-17 11:49:46 +0100

    Add files via upload

---
+++ ex3_14.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class ex3_14 {
+    public static void main(String[] args) {
+        int n, m, p, x, y;
+        Scanner teclat = new Scanner (System.in);
+        System.out.println("CÀLCUL D'ÀREES \n ------------- \n 1. Quadrat \n 2. Cercles \n 3. Triangle equilàter");
+        n = teclat.nextInt();
+       
+        if (n == 1) {
+            System.out.println("Escriu la mida del costat");
+            m = teclat.nextInt();
+            System.out.println("L'àrea del quadrat és: " + (m*m));
+        }
+        if (n == 2) {
+            System.out.println("Escriu la mida del radi");
+            p = teclat.nextInt();
+            System.out.println("L'àrea del cercle és: " + (Math.PI*(Math.pow(p, 2))));
+        }
+        if (n == 3) {
+            System.out.println("Escriu la mida de la base");
+            x = teclat.nextInt();
+            System.out.println("Escriu la mida de l'altura");
+            y = teclat.nextInt();
+            System.out.println("L'àrea del triangle és: " + (x*y/2));
+        }
+    }
+}
